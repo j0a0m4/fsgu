8fe1cb07c8284b59e08c00000b9ac1d1b01452ff
João Lopes <joaomarcos467@gmail.com>
2023-12-31 18:54:47 -0300

    refactor: changed impl to seq

--- bricks/Bricks.fs
+++ bricks/Bricks.fs
@@ -9,40 +9,59 @@ module Brick =
             StudRows: int
             Color: ConsoleColor
         }
-    
-    let studCount b = b.StudColumns * b.StudRows
+
+    let from (color: ConsoleColor) (cols: int, rows: int) =
+        {
+            Color = color
+            StudColumns = cols
+            StudRows = rows
+        }
+
+    let size b = b.StudColumns * b.StudRows
 
     let bricks =
         [|
-            (3, 2, ConsoleColor.Yellow)
-            (4, 2, ConsoleColor.Green)
-            (2, 1, ConsoleColor.Magenta)
-            (1, 1, ConsoleColor.Blue)
-            (2, 2, ConsoleColor.Red)
-            (4, 2, ConsoleColor.Blue)
-            (4, 2, ConsoleColor.Magenta)
-            (2, 2, ConsoleColor.Magenta)
-            (2, 2, ConsoleColor.Red)
-            (4, 2, ConsoleColor.Blue)
-            (3, 2, ConsoleColor.Magenta)
-            (4, 2, ConsoleColor.Green)
-            (3, 2, ConsoleColor.Red)
-            (4, 1, ConsoleColor.Blue)
-            (4, 2, ConsoleColor.Yellow)
-            (4, 2, ConsoleColor.Yellow)
-            (1, 1, ConsoleColor.Blue)
-            (1, 1, ConsoleColor.Green)
-            (2, 1, ConsoleColor.Yellow)
-            (4, 1, ConsoleColor.Magenta)
+            ConsoleColor.Yellow,
+            [|
+                3, 2
+                4, 2
+                4, 2
+                2, 1
+            |]
+            ConsoleColor.Green,
+            [|
+                4, 2
+                4, 2
+                1, 1
+            |]
+            ConsoleColor.Magenta,
+            [|
+                2, 1
+                4, 2
+                2, 2
+                3, 2
+                4, 1
+            |]
+            ConsoleColor.Blue,
+            [|
+                1, 1
+                4, 2
+                4, 2
+                4, 1
+                1, 1
+            |]
+            ConsoleColor.Red,
+            [|
+                2, 2
+                2, 2
+                3, 2
+            |]
         |]
-        |> Array.map (fun (col, row, color) ->
-            {
-                StudColumns = col
-                StudRows = row
-                Color = color
-            }
+        |> Seq.collect (fun (color, pairs) ->
+            pairs |> Seq.map (from color)
         )
-    
+        |> Seq.cache
+
     let toString (brick: Brick) =
         let rowChar =
             match brick.StudRows with
@@ -57,10 +76,10 @@ module Brick =
     let print (brick: Brick) =
         let (label, pattern) = brick |> toString
         printf "%s " label
-        
+
         Console.BackgroundColor <- brick.Color
         Console.ForegroundColor <- ConsoleColor.Black
         printf "[%s]" pattern
-        
+
         Console.ResetColor()
         printf " "
--- bricks/Program.fs
+++ bricks/Program.fs
@@ -4,44 +4,44 @@ open System
 printfn "All the bricks:"
 
 Brick.bricks
-|> Array.iter Brick.print
+|> Seq.iter Brick.print
 
 printfn "\n"
 
 printfn "Count of the bricks"
 Brick.bricks
-|> Array.length
+|> Seq.length
 |> printfn "Count: %i\n"
 
 printfn "Stud counts:"
 Brick.bricks
-|> Array.map Brick.studCount
-|> Array.iter (printf "%i;")
+|> Seq.map Brick.size
+|> Seq.iter (printf "%i;")
 
 printfn "\n"
 
 printfn "Red bricks"
 Brick.bricks
-|> Array.filter (fun b -> b.Color = ConsoleColor.Red)
-|> Array.iter Brick.print
+|> Seq.filter (fun b -> b.Color = ConsoleColor.Red)
+|> Seq.iter Brick.print
 
 printfn "\n"
 
 printfn "Group by color"
 Brick.bricks
-|> Array.groupBy _.Color
-|> Array.iter (fun (color, brick) ->
+|> Seq.groupBy _.Color
+|> Seq.iter (fun (color, brick) ->
     printfn "\n%s" (color.ToString())
-    brick |> Array.iter Brick.print
+    brick |> Seq.iter Brick.print
     printfn ""
 )
 
 printfn "Group by stud count"
 Brick.bricks
-|> Array.groupBy Brick.studCount
-|> Array.sortBy fst
-|> Array.iter (fun (count, brick) ->
+|> Seq.groupBy Brick.size
+|> Seq.sortBy fst
+|> Seq.iter (fun (count, brick) ->
     printf "%i " count
-    brick |> Array.iter Brick.print
+    brick |> Seq.iter Brick.print
     printfn ""
 )
